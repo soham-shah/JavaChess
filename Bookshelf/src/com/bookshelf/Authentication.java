4439df7e2865910b58a061c30a22beb8a5be5405
soham-shah <sohamshah225@gmail.com>
2016-12-03 17:00:28 -0700

    Adding administrator support
    adding admin support for the project

--- Bookshelf/src/com/bookshelf/Authentication.java
+++ Bookshelf/src/com/bookshelf/Authentication.java
@@ -2,34 +2,78 @@ package com.bookshelf;
 
 import java.util.ArrayList;
 
-/**
- * Created by soham on 12/3/16.
- */
 public class Authentication {
 
     private static ArrayList<User> users = new ArrayList<User>();
+    private static ArrayList<Admin> admins = new ArrayList<Admin>();
 
     public void addUser(User person){
         users.add(person);
     }
 
-    // TODO: 12/3/16 come back and implement an if check to actually know if that user is in the list of users
     public boolean removeUser(String username, String password){
         int location;
         for(int i = 0; i < users.size(); i++){
             if (users.get(i).getUsername() == username){
-                if (users.get(i).getPassword() == password)
+                if (users.get(i).getPassword() == password){
                     users.remove(i);
                     return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean validateUser (String username, String password){
+        for(int i = 0; i < users.size(); i++){
+            if (users.get(i).getUsername() == username){
+                if (users.get(i).getPassword() == password){
+                    return true;
+                }
             }
         }
         return false;
     }
 
     public void printUsers(){
-        for(int i = 0; i < users.size(); i++){
-            System.out.println(users.get(i).getUsername());
+        for(int i = 0; i < admins.size(); i++){
+            System.out.println(admins.get(i).getUsername());
         }
     }
+
+    public void addAdmin(Admin person){
+        admins.add(person);
+    }
+
+    public boolean removeAdmin(String username, String password){
+        int location;
+        for(int i = 0; i < admins.size(); i++){
+            if (admins.get(i).getUsername() == username){
+                if (admins.get(i).getPassword() == password)
+                    admins.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printAdmins(){
+        for(int i = 0; i < admins.size(); i++){
+            System.out.println(admins.get(i).getUsername());
+        }
+    }
+
+    public boolean validateAdmin (String username, String password){
+        for(int i = 0; i < admins.size(); i++){
+            if (admins.get(i).getUsername() == username){
+                if (admins.get(i).getPassword() == password){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 
 }
