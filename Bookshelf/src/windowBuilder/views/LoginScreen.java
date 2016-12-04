7a0ef11ce5468b2253420253aad1d6db3813f390
gramkuma <gira3678@colorado.edu>
2016-12-03 17:01:22 -0700

    Update Login
    @StattikRose

--- Bookshelf/src/windowBuilder/views/LoginScreen.java
+++ Bookshelf/src/windowBuilder/views/LoginScreen.java
@@ -5,10 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class LoginScreen {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -44,15 +48,29 @@ public class LoginScreen {
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setIcon(new ImageIcon("/Users/Girish/Desktop/Programming/JavaChess/Bookshelf/resources/booklogo.png"));
-		lblNewLabel.setBounds(68, 6, 302, 187);
+		lblNewLabel.setBounds(68, 6, 208, 128);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Username");
-		lblNewLabel_1.setBounds(46, 205, 61, 16);
+		lblNewLabel_1.setBounds(114, 176, 67, 16);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Password");
-		lblNewLabel_2.setBounds(46, 233, 61, 16);
+		lblNewLabel_2.setBounds(114, 204, 61, 16);
 		frame.getContentPane().add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(207, 171, 130, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(207, 199, 130, 26);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setBounds(171, 240, 88, 16);
+		frame.getContentPane().add(btnSubmit);
 	}
 }
