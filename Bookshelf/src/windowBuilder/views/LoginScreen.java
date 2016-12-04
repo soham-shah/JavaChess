3fc9e9f5ee712298f53dd3d63230325f34c5024f
StattikRose <Jennifer.Dooley@colorado.edu>
2016-12-03 19:23:27 -0700

    Completed Login Screen
    @soham-shah @spamiam03

--- Bookshelf/src/windowBuilder/views/LoginScreen.java
+++ Bookshelf/src/windowBuilder/views/LoginScreen.java
@@ -15,6 +15,13 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import java.awt.CardLayout;
+import java.awt.Font;
+import javax.swing.JLayeredPane;
+import javax.swing.border.BevelBorder;
+import java.awt.Color;
 
 public class LoginScreen {
 
@@ -50,34 +57,76 @@ public class LoginScreen {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 511, 767);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JPanel LogIn = new JPanel();
+		frame.getContentPane().add(LogIn, "name_13514574295050");
+		LogIn.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("/Users/Jen/Development/Bookshelf/JavaChess/Bookshelf/resources/booklogo.png"));
-		lblNewLabel.setBounds(144, 20, 208, 128);
-		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setEnabled(false);
+		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(118, 185, 248, 168);
+		LogIn.add(lblNewLabel);
+		lblNewLabel.setIcon(new ImageIcon("/Users/Jen/Development/Bookshelf/JavaChess/Bookshelf/resources/books.png"));
 		
 		JLabel lblNewLabel_1 = new JLabel("Username");
-		lblNewLabel_1.setBounds(114, 176, 67, 16);
-		frame.getContentPane().add(lblNewLabel_1);
+		lblNewLabel_1.setBounds(140, 382, 62, 16);
+		LogIn.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Password");
-		lblNewLabel_2.setBounds(114, 204, 61, 16);
-		frame.getContentPane().add(lblNewLabel_2);
+		lblNewLabel_2.setBounds(143, 420, 59, 16);
+		LogIn.add(lblNewLabel_2);
 		
 		textField = new JTextField();
-		textField.setBounds(207, 171, 130, 26);
-		frame.getContentPane().add(textField);
+		textField.setBounds(226, 377, 130, 26);
+		LogIn.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JPasswordField();
-		textField_1.setBounds(207, 199, 130, 26);
-		frame.getContentPane().add(textField_1);
+		textField_1.setBounds(226, 415, 130, 26);
+		LogIn.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setBounds(143, 462, 88, 29);
+		LogIn.add(btnSubmit);
+		
+		JButton btnNewUser = new JButton("New User");
+		btnNewUser.setBounds(243, 462, 103, 29);
+		LogIn.add(btnNewUser);
+		
+		JPanel UserScreen = new JPanel();
+		frame.getContentPane().add(UserScreen, "name_13531542104794");
+		UserScreen.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("Welcome to Bookshelf.me");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblNewLabel_3.setBounds(65, 28, 378, 73);
+		UserScreen.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon("/Users/Jen/Development/Bookshelf/JavaChess/Bookshelf/resources/bookshelf.png"));
+		lblNewLabel_4.setBounds(6, 6, 498, 733);
+		UserScreen.add(lblNewLabel_4);
+		
+		JPanel AdminScreen = new JPanel();
+		frame.getContentPane().add(AdminScreen, "name_13539955094575");
+		AdminScreen.setLayout(null);
+		btnNewUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = textField.getText();
+				char[] p = textField_1.getPassword();
+				String pass = new String(p);
+				Authentication auth = new Authentication();
+
+				auth.addUser(name, pass);
+			}
+		});
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String name = textField.getText();
@@ -97,21 +146,5 @@ public class LoginScreen {
 				}
 			}
 		});
-		btnSubmit.setBounds(114, 242, 88, 16);
-		frame.getContentPane().add(btnSubmit);
-		
-		JButton btnNewUser = new JButton("New User");
-		btnNewUser.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String name = textField.getText();
-				char[] p = textField_1.getPassword();
-				String pass = new String(p);
-				Authentication auth = new Authentication();
-
-				auth.addUser(name, pass);
-			}
-		});
-		btnNewUser.setBounds(231, 242, 88, 16);
-		frame.getContentPane().add(btnNewUser);
 	}
 }
